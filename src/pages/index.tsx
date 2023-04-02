1a1ebf998786728e21183f6961affef2f9824544
Emil Lystimaki <emilaleksanteri@gmail.com>
2023-04-02 23:00:20 +0100

    ui improvements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,73 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { api } from "~/utils/api";
+import { RouterOutputs, api } from "~/utils/api";
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime"
+import Image from 'next/image'
+
+dayjs.extend(relativeTime)
+
+const PostMessageWizard = () => {
+  const { user } = useUser()
+  if (user) {
+    return (
+      <div className="flex items-center">
+        <Image width={80} height={80} src={user.profileImageUrl} alt="Profile Picture" className="w-[80px] h-[80px] rounded-full drop-shadow-lg border-4 border-emerald-300" />
+        <div className="ml-8 w-full">
+          <div className="w-full flex items-center gap-8" >
+            <input placeholder="message..." className="w-[90%] p-4 overflow-scroll bg-inherit border-2 border-zinc-300 rounded-2xl outline-none" />
+            <button className="bg-emerald-300 text-xl p-4 rounded-2xl drop-shadow-lg pl-6 pr-6 font-bold">Send</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  return null
+}
+
+// very cool
+type MessageWithUser = RouterOutputs["message"]["getAll"][0]
+const MessageView = (message: MessageWithUser) => {
+  const user = useUser()
+  if (!message.author) return null
+
+  if (user.isSignedIn) {
+    return (
+      <div>
+        <div className="flex items-center gap-4">
+          {message.author?.id !== user.user.id && <Image width={48} height={48} src={message.author.profileImageUrl} alt="User" className="w-12 h-12 rounded-full text-xs drop-shadow-lg" />}
+          <div
+            className="p-4 bg-emerald-300 rounded-2xl flex flex-col items-center w-fit drop-shadow-xl flex gap-4">
+            <p className="text-lg" >{message.message.body}</p>
+          </div>
+          {message.author?.id === user.user.id && <Image width={48} height={48} src={message.author.profileImageUrl} alt="profile" className="w-12 h-12 rounded-full text-xs drop-shadow-lg" />}
+        </div>
+        <p className="text-[11px] text-zinc-400 mt-2 ml-1">{dayjs(message.message.sentAt).fromNow()}</p>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <div className="flex items-center gap-4">
+        <Image width={48} height={48} src={message.author.profileImageUrl} alt="profile" className="w-12 h-12 rounded-full text-xs drop-shadow-lg" />
+        <div
+          className="p-4 bg-emerald-300 rounded-2xl flex flex-col items-center w-fit drop-shadow-xl flex gap-4">
+          <p className="text-lg" >{message.message.body}</p>
+        </div>
+      </div>
+      <p className="text-[11px] text-zinc-400 mt-2">{dayjs(message.message.sentAt).fromNow()}</p>
+    </div>
+  )
+}
+
 
 const Home: NextPage = () => {
   const user = useUser()
   const {data} = api.message.getAll.useQuery()
+
+  if (!user) return null
 
   return (
     <>
@@ -34,14 +96,31 @@ const Home: NextPage = () => {
         }
         </div>
         <section className="relative w-[50%] bg-zinc-200 h-96 overflow-scroll rounded-2xl">
-          <div className="absolute flex flex-col gap-8 bottom-10 right-8">
-            {data?.map((message) => (
-              <div key={message.id} className="p-4 bg-emerald-300 rounded-2xl flex justify-center items-center w-fit text-lg">
-                {message.body}
-              </div>
-            ))}
+          <div className="absolute flex flex-col gap-8 bottom-10">
+            {data?.map((message) => {
+              if (user.isSignedIn && user.user.id === message.author?.id) {
+               return (
+                <div className="ml-[800px]"><MessageView key={message.message.id} {...message} /></div>
+                
+              )}
+
+              if (user.isSignedIn && user.user.id !== message.author?.id) {
+                return (
+                 <div className="ml-6"><MessageView key={message.message.id} {...message} /></div>
+                 
+               )}
+
+              return (
+                <div className="ml-6"><MessageView key={message.message.id} {...message} /></div>
+              )
+            })}
           </div>
         </section>
+        {user.isSignedIn &&
+        <div className="bg-zinc-200 mt-4 w-[50%] p-4 rounded-2xl">
+          <PostMessageWizard />
+        </div>
+        }
       </main>
     </>
   );
--- src/server/api/routers/message.ts
+++ src/server/api/routers/message.ts
@@ -1,9 +1,33 @@
-import { z } from "zod";
+import { User, clerkClient } from '@clerk/nextjs/dist/api';
+import { z } from 'zod';
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, publicProcedure } from '~/server/api/trpc';
+
+// clerck returns a lot of fields, need to be narrowed down
+const filterUserForClient = (users: User) => {
+  return {
+    id: users.id,
+    username: users.username,
+    profileImageUrl: users.profileImageUrl,
+  };
+};
 
 export const messagesRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.message.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const messages = await ctx.prisma.message.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: messages.map((message) => message.userId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    return messages.map((message) => ({
+      message,
+      author: users.find((user) => user.id === message.userId),
+    }));
   }),
 });
