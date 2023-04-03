d0e181e40ec1223d6ad4b525b5205010143910c4
Emil Lystimaki <emilaleksanteri@gmail.com>
2023-04-03 01:17:49 +0100

    loading state on post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,9 +47,12 @@ const PostMessageWizard = () => {
             <button className="bg-emerald-300 text-xl p-4 rounded-2xl drop-shadow-lg pl-6 pr-6 font-bold"
               onClick={() => mutate({ body: input })}
             >
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-              </svg>
+              {isPosting && <Loader widthHeight="h-[24px] w-[24px]" />}
+              {!isPosting &&
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+                </svg>
+              }
             </button>
           </div>
         </div>
